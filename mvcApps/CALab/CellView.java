f95e7fa75bc900cb23f5720dbfed1b4007658667
Claireran520 <clairexiaokeran@gmail.com>
2024-03-11 16:29:14 -0700

    View java files uploaded

--- mvcApps/CALab/CellView.java
+++ mvcApps/CALab/CellView.java
@@ -1,41 +1,54 @@
 package CALab;
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-
-import CALab.Cell;
 import mvc.*;
-
 
 public class CellView extends JButton implements ActionListener, Subscriber {
     private Cell myCell;
+    private int row;
+    private int col;
 
     public CellView(Cell c) {
         myCell = c;
-        if (c != null) { c.subscribe(this); }
+        if (c != null) {
+            c.subscribe(this);
+        }
         this.addActionListener(this);
     }
 
-    public CellView() { this(null); }
+    public CellView() {
+        this(null);
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public void setCol(int col) {
+        this.col = col;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getCol() {
+        return col;
+    }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         myCell.nextState();
-        // call update needed?
-    }
-
-    // called by notifySubscribers and GridView.update
-
-    //@Override
-    public void update(String msg, Object oldState, Object newState) {
         setBackground(myCell.getColor());
-        setBorder(BorderFactory.createLineBorder(Color.black)); // needed?
+        setBorder(BorderFactory.createLineBorder(Color.black));
         setText("" + myCell.getStatus());
     }
 
     @Override
-    public void update() {
-        // TODO flush me out
+    public void update(String msg, Object oldState, Object newState) {
+        setBackground(myCell.getColor());
+        setBorder(BorderFactory.createLineBorder(Color.black));
+        setText("" + myCell.getStatus());
     }
 }
