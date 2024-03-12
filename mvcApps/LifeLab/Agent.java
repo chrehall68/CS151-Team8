1ed758f80c38f104c3f5356bef23a85c9902a6bd
Alex Ross <adross1027@gmail.com>
2024-03-12 13:40:21 -0700

    life lab created

---
+++ mvcApps/LifeLab/Agent.java
@@ -0,0 +1,24 @@
+package LifeLab;
+
+import CALab.Cell;
+
+public class Agent extends Cell {
+
+    private int status = 0;
+    private int ambience = 8;
+
+    public void update() {}
+
+    public void nextState() {}
+
+    public void observe() {}
+
+    public void interact() {}
+
+    public int getStatus() {
+        return 0;
+    }
+
+    public void reset(boolean randomly) {}
+
+}
