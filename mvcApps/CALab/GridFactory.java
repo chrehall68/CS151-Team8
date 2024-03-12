198f0151f7c67997bebee52d16bdcbb94be1ec9d
chrehall68 <chrehall68@gmail.com>
2024-03-12 10:54:01 -0700

    Match case, test

--- mvcApps/CALab/GridFactory.java
+++ mvcApps/CALab/GridFactory.java
@@ -8,7 +8,7 @@ import mvc.View;
 public abstract class GridFactory implements AppFactory {
     @Override
     public View makeView(Model m) {
-        return new GridView((Grid)m);
+        return new GridView(m);
     }
 
     @Override
@@ -21,7 +21,7 @@ public abstract class GridFactory implements AppFactory {
 
     @Override
     public String[] getHelp() {
-        return new String[]{"Click Run1 to update each cell once.", "Click Run 50 to update each cell 50 times.", "Click populate to set each cell to a random value.", "Click clear to set each cell to an initial value."};
+        return new String[]{"Click Run1 to update each cell once.", "Click Run50 to update each cell 50 times.", "Click Populate to set each cell to a random value.", "Click Clear to set each cell to an initial value."};
     }
 
     @Override
@@ -31,16 +31,16 @@ public abstract class GridFactory implements AppFactory {
 
     @Override
     public String[] getEditCommands() {
-        return new String[]{"Run1", "Run 50", "Populate", "Clear"};
+        return new String[]{"Run1", "Run50", "Populate", "Clear"};
     }
 
     @Override
     public Command makeEditCommand(Model model, String type, Object source) {
 
-        if (type == "run1") return new RunCommand.Run1(model);
-        else if (type == "run50") return new RunCommand.Run50(model);
-        else if (type == "clear") return new ClearCommand(model);
-        else if (type == "populate") return new PopulateCommand(model);
+        if (type.equals("Run1")) return new RunCommand.Run1(model);
+        else if (type.equals("Run50")) return new RunCommand.Run50(model);
+        else if (type.equals("Clear")) return new ClearCommand(model);
+        else if (type.equals("Populate")) return new PopulateCommand(model);
         else return null;
     }
 }
--- mvcTests/CALabTests/ConcreteFactory.java
+++ mvcTests/CALabTests/ConcreteFactory.java
@@ -2,46 +2,13 @@ package CALabTests;
 
 import CALab.GridFactory;
 import CALab.GridPanel;
-import CALab.GridView;
-import mvc.Command;
 import mvc.Model;
-import mvc.View;
 
 
 public class ConcreteFactory extends GridFactory {
     @Override
-    public View makeView(Model m) {
-        return new GridView(m);
-    }
-
-    @Override
     public Model makeModel() {
         return new ConcreteGrid();
-    }
-
-    @Override
-    public String getTitle() {
-        return "Test";
-    }
-
-    @Override
-    public String[] getHelp() {
-        return new String[]{"Hi"};
-    }
-
-    @Override
-    public String about() {
-        return "An example test";
-    }
-
-    @Override
-    public String[] getEditCommands() {
-        return new String[]{"Cool"};
-    }
-
-    @Override
-    public Command makeEditCommand(Model model, String name, Object source) {
-        return null;
     }
 
     public static void main(String[] args) {
